55686bde2cef48714dd68b16167fee75c1fb5bf0
Sujit Biswas <livecodebase@gmail.com>
2023-05-23 16:17:14 +0530

    fix

--- index.js
+++ index.js
@@ -2,31 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 
-const client = require('twilio')(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/api/messages', (req, res) => {
-  res.header('Content-Type', 'application/json');
-  client.messages
-    .create({
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: req.body.to,
-      body: req.body.body
-    })
-    .then(() => {
-      res.send(JSON.stringify({ success: true }));
-    })
-    .catch(err => {
-      console.log(err);
-      res.send(JSON.stringify({ success: false }));
-    });
-});
+const product = require("./api/message");
+
+app.use("/api/messages", product);
 
 app.use('/', express.static('build'))
 const PORT = process.env.PORT || 8080;
